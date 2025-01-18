eaf0d368c63e503f937cd4d467251001f75b4a45
Nikhil Anand <102014379+Nikhil-gna@users.noreply.github.com>
2025-01-18 18:26:42 +0530

    enabled cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"; // Import the cors package
 import connectDB from "./Config/database";
 import employeeRoutes from "./Routes/EmployeeOBRoutes";
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+// Enable CORS
+app.use(cors());
 
 // Connect to the database
 connectDB();
